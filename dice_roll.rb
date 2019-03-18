1146a47d4e588381b04ab0c8545ef883bebca966
mario rodrigueza <mariorodriguezan@gmail.com>
2019-03-18 16:10:31 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,13 +2,6 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
-
-def roll2
-  rolls = []
-    10.times do
-      rolls << roll
-    end
-    puts roll
+def roll
+ return rand(1..6)
 end
-
-roll2
